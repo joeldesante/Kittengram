c1ffa203ba8af617dd0d7d1ac72e4cda35ae483c
Joel DeSante <joeldesante@gmail.com>
2024-01-30 13:21:02 +0000

    Add KittenUser Service

---
+++ Services/KittenUserService/Models/User.cs
@@ -0,0 +1,9 @@
+namespace KittenUserService.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string ProfileImageId { get; set; }
+    public string Username { get; set; }
+    public string Biography  { get; set; }
+}
---
+++ Services/KittenUserService/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using KittenUserService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<UserContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
